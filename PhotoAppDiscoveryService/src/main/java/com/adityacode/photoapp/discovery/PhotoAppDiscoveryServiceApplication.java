da41fb79459dbcaf989039494d59bd01d63c56a1
adityaraj <adityaraj@localhost>
2022-09-26 16:22:28 +0530

    Eureka Discovery Server created

--- PhotoAppDiscoveryService/src/main/java/com/adityacode/photoapp/discovery/PhotoAppDiscoveryServiceApplication.java
+++ PhotoAppDiscoveryService/src/main/java/com/adityacode/photoapp/discovery/PhotoAppDiscoveryServiceApplication.java
@@ -2,8 +2,10 @@ package com.adityacode.photoapp.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class PhotoAppDiscoveryServiceApplication {
 
 	public static void main(String[] args) {
